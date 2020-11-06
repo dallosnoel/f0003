ac532f01183dab64aa357ac9dc42a4f20d2e15c7
dallosnoel <dallosnoel@gmail.com>
2020-11-06 07:28:17 +0000

    Initial commit

---
+++ F0003a.py
@@ -0,0 +1,4 @@
+vezetéknév = input('Mi a vezetékneved? ')
+keresztnév = input('Mi a keresztneved? ')
+
+print(f'A te neved {vezetéknév} {keresztnév}.')
---
+++ F0003b.py
@@ -0,0 +1,7 @@
+vezetéknév = input('Mi a vezetékneved? ')
+keresztnév = input('Mi a keresztneved? ')
+születésiév = input('Mi a születési éved? ')
+csillagjegy = input('Mi a csillagjegyed? ')
+
+print(f'A te neved {vezetéknév} {keresztnév}.')
+print(f'{születésiév}-ben születtél, a(z) {csillagjegy} csillagjeggyel.')
